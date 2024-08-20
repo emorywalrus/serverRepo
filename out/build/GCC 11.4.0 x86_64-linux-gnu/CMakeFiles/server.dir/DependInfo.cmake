79d5f4648eaedfeb17a5125fe372e819acb87280
Emory <emorywrussell@gmail.com>
2024-08-19 21:07:21 -0500

    update again

---
+++ out/build/GCC 11.4.0 x86_64-linux-gnu/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "server_autogen/timestamp" "custom" "server_autogen/deps"
+  "/home/emory/serverRepo/main.cpp" "CMakeFiles/server.dir/main.cpp.o" "gcc" "CMakeFiles/server.dir/main.cpp.o.d"
+  "/home/emory/serverRepo/out/build/GCC 11.4.0 x86_64-linux-gnu/server_autogen/mocs_compilation.cpp" "CMakeFiles/server.dir/server_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/server.dir/server_autogen/mocs_compilation.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install build-essential cmake gdb qt6-base-dev
